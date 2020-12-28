9ce6ff5e11c3f41171470a895b90e21dde09d9ec
eskaya <esra.kaya@bil.omu.edu.tr>
2020-12-28 12:19:48 +0300

    start project

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = function() {
+    return {
+        products:[
+            {id:1, name:"Samsung S5", price:1000, imageUrl:"1.jpg", description:"İyi telefon"},
+            {id:2, name:"Samsung S6", price:2000, imageUrl:"2.jpg", description:"Güzel  telefon"},
+            {id:3, name:"Samsung S7", price:3000, imageUrl:"3.jpg", description:"Kötü telefon"},
+            {id:4, name:"Samsung S8", price:4000, imageUrl:"4.jpg", description:"İyi telefon"},
+            {id:5, name:"Samsung S9", price:5000, imageUrl:"5.jpg", description:"İyi telefon"},
+            {id:6, name:"Samsung S10", price:6000, imageUrl:"6.jpg", description:"İyi telefon"},            
+        ],
+        categories:[
+            {id:"1", name:"Telefon"},
+            {id:"2", name:"Bilgisayar"},
+            {id:"3", name:"Elektronik"}
+        ],
+        orders:[
+
+        ]
+    }
+}
